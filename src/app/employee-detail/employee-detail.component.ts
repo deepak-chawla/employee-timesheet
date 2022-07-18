72b39b33bbefb9840bd494b3d18d33736e591109
deepak-chawla <deepakchawla078@gmail.com>
2022-07-18 17:55:53 +0500

    updated

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -85,6 +85,7 @@ export class EmployeeDetailComponent implements OnInit {
     this.taskForm.value.Tasks.forEach((task: Task) => {
       if (task.tid == null) {
         task.employee = this.selectedEmployee;
+        task.date = new Date();
       }
       this.taskService.updateEmployeeTask(task).subscribe();
     });
